0adcd85eed2d79ffdcb1d5cec49801b9adfc9ddb
Florian <florian.martin3@etu.univ-lyon1.fr>
2016-04-27 22:28:58 +0200

    New run

--- src/Main.java
+++ src/Main.java
@@ -3,69 +3,48 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-	public ArrayList<Agence> ListAgence;
-	public ArrayList<LieuFormation> ListFormation;
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Mise en place de l'algo et lancement du Run
+
 		System.out.println("Faire quel test?  ");
 		System.out.println("a : Test avec le jeu de 100 Agences");
 		System.out.println("b: Test avec le jeu de 300 Agences");
 		System.out.println("c : Test avec le jeu de 500 Agences");
-		System.out.println("d : Test avec le jeu de 500 Agences");
+		System.out.println("d : Test avec votre jeu d'Agences");
 		System.out.println("e : Quitter");
 		Scanner scan = new Scanner(System.in);
 		String choice = scan.nextLine();
+		Run myRun = new Run();
 
-		try {
-			File f = new File("src/Database/LieuxPossibles.txt");
-			FileReader fr = new FileReader(f);
-			BufferedReader br = new BufferedReader(fr);
+		do {
+			switch (choice) {
+			case "a" :				
+				myRun.run("src/Database/ListeAgences_100.txt");
+				break;
+				
+			case "b" :				
+				myRun.run("src/Database/ListeAgences_300.txt");
+				break;
 			
-			try {
-				String line;
-				String word[];
-				line = br.readLine();
-				while(true) {
-					
-				line = br.readLine();	
-				if (line != null){	
-					
-					word = line.split(";");									
-				}
-				
-					
+			case "c" :				
+				myRun.run("src/Database/ListeAgences_500.txt");
+				break;		
 			
-				else 
-					break;	
-				
-				
-				}
-			} catch (IOException exception) {
-
-				System.out.println("Erreur lors de la lecture : " + exception.getMessage());
-
+			/*
+			 * Remplacer Votrefichier par le nom de votre fichier txt
+			 */
+			case "d" :							
+				myRun.run("src/Database/Votrefichier.txt");
+				break;
 			}
-			
-			do {
-				switch(choice){
-					
-				}
-				
-			}while(choice == "e");
-			
-			
-			
-			
 
-		} catch (FileNotFoundException exception) {
-			System.out.println("Le fichier n'a pas été trouvé");
-		}
+		} while (choice == "e");
 
 	}
 
---
+++ src/Run.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Run {
+
+	public ArrayList<Agence> ListAgence = new ArrayList<Agence>();
+	public ArrayList<LieuFormation> ListFormation = new ArrayList<LieuFormation>();
+
+	public void run(String choice) {
+
+		try {
+			File f = new File("src/Database/LieuxPossibles.txt");
+			FileReader fr = new FileReader(f);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			String word[];
+
+			try {
+
+				line = br.readLine();
+				while (true) {
+
+					line = br.readLine();
+					if (line != null) {
+						word = line.split(";");
+						LieuFormation current = new LieuFormation(word[0], Integer.parseInt(word[1]),
+								Integer.parseInt(word[2]), Integer.parseInt(word[3]));
+						ListFormation.add(current);
+					}
+
+					else
+						break;
+				}
+
+			}
+
+			catch (IOException exception) {
+
+				System.out.println("Erreur lors de la lecture : " + exception.getMessage());
+
+			}
+
+			f = new File("src/Database/LieuxPossibles.txt");
+			fr = new FileReader(f);
+			br = new BufferedReader(fr);
+
+			try {
+
+				line = br.readLine();
+				while (true) {
+
+					line = br.readLine();
+					if (line != null) {
+						word = line.split(";");
+						LieuFormation current = new LieuFormation(word[0], Integer.parseInt(word[1]),
+								Integer.parseInt(word[2]), Integer.parseInt(word[3]));
+						ListFormation.add(current);
+					}
+
+					else
+						break;
+
+				}
+			} catch (IOException exception) {
+
+				System.out.println("Erreur lors de la lecture : " + exception.getMessage());
+
+			}
+
+		} catch (FileNotFoundException exception) {
+			System.out.println("Le fichier n'a pas été trouvé");
+		}
+
+	}
+}
