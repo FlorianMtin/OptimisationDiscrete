3e2468da28de352aad8af8c5b1ee30f587ee866d
Florian <florian.martin3@etu.univ-lyon1.fr>
2016-04-27 20:57:33 +0200

    Début lecture fichier txt

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
 
+	// public ArrayList<Agence> ListAgence;
+	// public ArrayList<LieuFormation> ListFormation;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Mise en place de l'algo et lancement du Run 
-		
+		// Mise en place de l'algo et lancement du Run
+
+		try {
+			File f = new File("LieuxPossibles.txt");
+			FileReader fr = new FileReader(f);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			try {
+				line = br.readLine();
+				System.out.println(line);
+			} catch (IOException exception) {
+
+				System.out.println("Erreur lors de la lecture : " + exception.getMessage());
+
+			}
+
+		} catch (FileNotFoundException exception) {
+			System.out.println("Le fichier n'a pas été trouvé");
+		}
 
 	}
 
--- src/Run.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class Run {
-	public ArrayList<Agence> ListAgence;
-	public ArrayList<LieuFormation> ListFormation; 
-	
-}
