4c1a63c82e77645ec204ac7420375a22227fe9a6
Florian <florian.martin3@etu.univ-lyon1.fr>
2016-04-27 20:18:27 +0200

    Création Main + Run

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Mise en place de l'algo et lancement du Run 
+		
 
 	}
 
