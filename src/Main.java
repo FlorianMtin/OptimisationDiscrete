f421d90923f2e929de2bb4b29951bc04316b53fb
Florian <florian.martin3@etu.univ-lyon1.fr>
2016-04-27 21:34:01 +0200

    Push main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -9,20 +13,44 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Mise en place de l'algo et lancement du Run
+		System.out.println("Faire quel test?  ");
+		System.out.println("a : ");
+		System.out.println("?: Display");
+		System.out.println("q: Quit");
+		Scanner scan = new Scanner(System.in);
+		String choice = scan.nextLine();
 
 		try {
-			File f = new File("LieuxPossibles.txt");
+			File f = new File("src/Database/LieuxPossibles.txt");
 			FileReader fr = new FileReader(f);
 			BufferedReader br = new BufferedReader(fr);
-			String line;
+			
 			try {
+				String line;
+				String word[];
 				line = br.readLine();
-				System.out.println(line);
+				while(true) {
+					
+				line = br.readLine();	
+				if (line != null){	
+					
+					word = line.split(";");									
+				}
+				
+					
+			
+				else 
+					break;	
+				
+				
+				}
 			} catch (IOException exception) {
 
 				System.out.println("Erreur lors de la lecture : " + exception.getMessage());
 
 			}
+			
+			
 
 		} catch (FileNotFoundException exception) {
 			System.out.println("Le fichier n'a pas été trouvé");
