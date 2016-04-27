982cd33b12384bc94b8ed8b0e2f5be0f8941d7fb
Adrien <Adrien@Adrien_Portable>
2016-04-27 21:29:41 +0200

    Création Lieu Agence LieuFormation Méthode de calcul distance

---
+++ OptiDisAdFlo/src/Lieu.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Adrien
+ */
+public abstract class Lieu {
+    protected float longitude;
+    protected float latitude;
+    protected int codepostale;
+    protected String name;
+    
+    public Lieu (String name, int codepostale, int longitude, int latitude)
+    {
+        this.name = name;
+        this.codepostale = codepostale;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+    
+    public int distance (Lieu autreLieu)
+    {
+        
+        
+        double R = 6371; //Rayon de la Terre moyen
+        double a = Math.toRadians(this.latitude);
+        double b = Math.toRadians(autreLieu.latitude);
+        double c = Math.toRadians(autreLieu.longitude - this.longitude);
+        double d = Math.toRadians(autreLieu.latitude - this.latitude);
+
+        double dist = Math.sin(d/2)*Math.sin(d)/2+Math.cos(a)*Math.cos(b)*Math.sin(c/2)*Math.sin(c/2);
+        return (int) ((int) R * (2 * Math.atan2(Math.sqrt(dist), Math.sqrt(1 - dist))));
+    }
+}
