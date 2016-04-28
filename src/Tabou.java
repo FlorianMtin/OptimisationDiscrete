4b03e2aa9ced8ea280918dafe9e13afa542531b2
Florian <florian.martin3@etu.univ-lyon1.fr>
2016-04-28 15:34:08 +0200

    Ajout MeilleurVoisin

--- src/Tabou.java
+++ src/Tabou.java
@@ -1,60 +1,73 @@
 import java.util.ArrayList;
 
-
 public class Tabou {
-	
+
 	private ArrayList<Agence> listeTabou = new ArrayList<Agence>();
 	private ArrayList<Integer> valueVoisinage = new ArrayList<Integer>();
-	private int iteration ;
+	private ArrayList<Agence> meilleurSolution = new ArrayList<Agence>();
+	private int iteration;
 	private int tailleTabou;
-	private float meilleurResultat  = 0;
-	
-	/* 
+	private float meilleurResultat = 0;
+
+	/*
 	 * Initialisation de la méthode Tabou
 	 */
-	public Tabou(int iteration, int tailleT){
+	public Tabou(int iteration, int tailleT) {
 		this.iteration = iteration;
 		this.tailleTabou = tailleT;
-		
+
 	}
-	
+
 	/*
 	 * Running de la méthode Tabou
 	 */
-	public void runTabou(ArrayList<Agence> LA, ArrayList<LieuFormation> LF){
-		//System.out.println(iteration + "  " + tailleTabou);
-		
-		solutionInit(LA,LF); // On répartie aléatoirement les agences dans les lieux de formation
-		Integer indiceElu ;
+	public void runTabou(ArrayList<Agence> LA, ArrayList<LieuFormation> LF) {
+		// System.out.println(iteration + " " + tailleTabou);
+
+		solutionInit(LA, LF); // On répartie aléatoirement les agences dans les
+								// lieux de formation
+		Integer indiceElu;
 		ArrayList<Agence> temp = new ArrayList<Agence>();
-		for ( int i = 0 ; i < iteration; i++){
-			
-			temp = LA; 
+		for (int i = 0; i < iteration; i++) {
+
+			temp = LA;
 			temp.getVoisinage(valueVoisinage);
 			indiceElu = meilleurVoisin(valueVoisinage);
-			
-			if ( valueVoisinage.get(indiceElu) > meilleurResultat) {
-				if (listeTabou.size() >=tailleTabou){
+
+			if (valueVoisinage.get(indiceElu) > meilleurResultat) {
+				if (listeTabou.size() >= tailleTabou) {
 					listeTabou.remove(0);
 					listeTabou.add(temp.get(indiceElu));
-					
+
+				} else
+					listeTabou.add(temp.get(indiceElu));
+			} else {
+				if (valueVoisinage.get(indiceElu) < meilleurResultat) {
+					LA.remove(indiceElu);
+					LA.add(indiceElu, temp.get(indiceElu));
+					temp.clear();
 				}
 			}
-			
-			
-			
+
 		}
-		
-		
-		
+
 	}
-	
-	public void solutionInit(ArrayList<Agence> LA, ArrayList<LieuFormation> LF){
-		
+
+	public void solutionInit(ArrayList<Agence> LA, ArrayList<LieuFormation> LF) {
+
 	}
-	
-	public int meilleurVoisin(ArrayList<Integer> value){
-		return 0;
+
+	public int meilleurVoisin(ArrayList<Integer> value) {
+		int max = Integer.MIN_VALUE;
+		int solution = 0;
+		for (int i = 0; i < value.size(); i++) {
+			if (max < value.get(i)) {
+				solution = i;
+				max = value.get(i);
+			}
+
+		}
+		return solution;
 	}
 
 }
